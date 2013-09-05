cdb78bba3a66b72fa0c5e6b20d04f863d54daacd
Song Qiang <tsiangsung@gmail.com>
2013-09-05 12:24:41 -0700

    add description of usage and example

--- logrun.sh
+++ logrun.sh
@@ -1,3 +1,6 @@
+# logrun: a function to run a bash coomand and log the running info
+# Song Qiang <keeyang@ustc.edu> 2013
+
 function logrun # command logfile
 {
     if [ "$#" == 2 ];
@@ -8,3 +11,17 @@ function logrun # command logfile
         echo "logrun: command logfile" 1>&2;
     fi
 }
+
+# this function expects two arguments: the first being the command to run and 
+# the second being the logfile. 
+# if the command contains spaces, it should be quoted.
+
+# use case:
+# qiangson@hpc-cmb:~/tmp
+# $ logrun "echo test" test.log
+# >>>  echo test
+# test
+# qiangson@hpc-cmb:~/tmp
+# $ echo test.log
+# test.log
+
